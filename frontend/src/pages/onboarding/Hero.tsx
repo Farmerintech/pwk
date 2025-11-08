d884eabb697c8fdfec94ed3371179f6ed1e85cc5
farmerintech <farmerintech@gmail.com>
2025-11-08 16:23:34 +0100

    added hall of fame to the home page

--- frontend/src/pages/onboarding/Hero.tsx
+++ frontend/src/pages/onboarding/Hero.tsx
@@ -8,77 +8,15 @@ import Hero5 from "../../assets/dice.png";
 
 
 export const HeroSection = () => {
-
   return (
+    <section className="relative w-full bg-black overflow-hidden z-10">
+      <div className="starfield"></div>
 
-
-    <section
-      className="w-full bg-cover bg-center bg-black flex flex-col justify-between ">
-
-
-{/* <HeroSection2/> */}
-<HeroSection3/>
-
+      <HeroSection3 />
     </section>
-
   );
 };
 
-
-
-
-
-
-
-
-
-
-export const HeroSection2 = () => {
-  const baseImgClass =
-    "w-[60px] mt-40 h-[60px] md:w-[100px] md:h-[100px] border-4 rounded-[30px] absolute object-cover";
-
-  const radius = 200; // Adjust for how far images sit from the center
-
-  type CSSWithVars = React.CSSProperties & { [key: string]: string | number };
-
-  const images = [
-    { src: Hero, border: "border-purple-500", deg: 0 },
-    { src: Hero2, border: "border-red-500", deg: 60 },
-    { src: Hero3, border: "border-blue-500", deg: 120 },
-    { src: Hero4, border: "border-yellow-500", deg: 180 },
-    { src: Hero3, border: "border-green-500", deg: 240 },
-    { src: Hero1, border: "border-pink-500", deg: 300 },
-  ];
-
-  return (
-    <div className="relative flex flex-col bg-black items-center justify-center h-[500px] md:h-[700px] px-4 md:px-20 overflow-hidden md:mt-20">
-      {/* Circle Orbit - Centered */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] md:w-[600px] md:h-[600px] relative">
-        {images.map((item, i) => (
-          <img
-            key={i}
-            src={item.src}
-            alt={`Hero${i + 1}`}
-            className={`${baseImgClass} ${item.border}`}
-            style={
-              {
-                "--r": `${radius}px`,
-                transform: `rotate(${item.deg}deg) translate(var(--r)) rotate(-${item.deg}deg)`,
-              } as CSSWithVars
-            }
-          />
-        ))}
-      </div>
-
-      {/* Center Text */}
-      <div className="text-center max-w-[90%] md:max-w-[65%] lg:px-20 z-20">
-        <p className="text-white text-[28px] sm:text-[28px] md:text-[40px] lg:text-[52px] font-semibold leading-tight">
-          Uniting Kwara Youths Through Sports, Creativity & Purpose.
-        </p>
-      </div>
-    </div>
-  );
-};
 
 
 import { useEffect, useRef, useState } from "react";
