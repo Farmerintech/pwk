c7a098d73d2fa9ff4103e6dd1dcabbde4f071fd3
farmerintech <farmerintech@gmail.com>
2025-11-12 08:41:20 +0100

    upload dist

--- frontend/src/pages/auth/adminSignIn.tsx
+++ frontend/src/pages/auth/adminSignIn.tsx
@@ -42,7 +42,7 @@ export const AdminSignIn: React.FC = () => {
 
   const { mutate, isPending, error, isError } = useMutation<IUSER>({
     mutationFn: async () => {
-      const res = await fetch(`https://pwk.onrender.com/api/auth/user/login`, {
+      const res = await fetch(`https://pwk.onrender.com/api/auth/admin/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
